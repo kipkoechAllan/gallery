3ef7d6d7c40b49f0c9e00722dc13fd430abc8f81
kipkoechAllan <koecheallan@gmail.com>
2023-06-27 01:32:41 +0300

    changes

--- _config.js
+++ _config.js
@@ -2,14 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-<<<<<<< HEAD
-    production: 'mongodb+srv://Allan:33492330>@cluster0.whje4pz.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://Allan:33492330@cluster0.whje4pz.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://Allan:33492330@cluster0.whje4pz.mongodb.net/darkroom-test?retryWrites=true&w=majority',
-=======
-    production: 'mongodb+srv://Allan:33492330@cluster0.whje4pz.mongodb.net/?retryWrites=true&w=majority',
-    development: 'mongodb+srv://Allan:33492330@cluster0.whje4pz.mongodb.net/?retryWrites=true&w=majority',
-    test: 'mongodb+srv://Allan:33492330@cluster0.whje4pz.mongodb.net/?retryWrites=true&w=majority',
->>>>>>> master
+    production: 'mongodb+srv://user-Gallery:33492330>@cluster0.whje4pz.mongodb.net/darkroom?retryWrites=true&w=majority',
+    development: 'mongodb+srv://user-Gallery:33492330@cluster0.whje4pz.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://user-Gallery@cluster0.whje4pz.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
 module.exports = config;
